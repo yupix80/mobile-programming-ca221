d709b6c723f3e66f66b002b4b332c925cd9e424c
schetykaz <yupiterkananda98@gmail.com>
2024-11-04 00:46:05 +0800

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/widgets/post_item.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final items = List.generate(30, (index) => const PostItem());
+    return SingleChildScrollView(
+      child: Column(
+        children: items,
+      ),
+    );
+  }
+}
 
